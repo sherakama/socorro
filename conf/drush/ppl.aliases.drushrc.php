c4600824e79b52012c6102b7baf73ba3f62cb0ac
Shea McKinney <sherakama@gmail.com>
2018-02-06 07:12:38 -0800

    fixup! wip.

---
+++ conf/drush/ppl.aliases.drushrc.php
@@ -0,0 +1,33 @@
+<?php
+
+## STANFORD WILDCARD #######################################################
+// The command you just typed in shell.
+$command = $_SERVER['argv'];
+// Look at every argument...
+foreach ($command as $arg){
+  // There aren't many cases where there are '.'...
+  $test = explode('.',$arg);
+  $first = array_shift($test);
+  $remote_user = $_ENV['SUNET'];
+  switch($first) {
+    case "@ppl":
+      // Set the project to be whatever the alias was
+      $project_alias = str_replace('@', '', $arg);
+      $project_name = "dp_" . array_pop($test);
+      $shortname = str_replace("dp_", "", $project_name);
+      $uri = "https://people.stanford.edu/" . $shortname;
+      $remote_host = "people1.stanford.edu";
+      break;
+  }
+}
+
+// project alias; this will be sse.ds_foo, uat.ds_foo, or ppl.dp_sunetid
+$aliases[$project_alias] = array(
+  'remote-host' => $remote_host,
+  'remote-user' => $remote_user,
+  'root' => '/var/www/'. $project_name . '/public_html',
+  'uri' => $uri,
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+  ),
+);
